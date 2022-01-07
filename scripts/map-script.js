23aa05d71bf3114f943c01e1b2f8e5e04514ddbb
czpmk <czapiewskimk@gmail.com>
2022-01-08 00:46:11 +0100

    add popups and functions

--- scripts/map-script.js
+++ scripts/map-script.js
@@ -1,12 +1,12 @@
 window.onload = init;
 
-let eti_coords = { "lon": 54.37165722576064, "lat": 18.612704654420106 }
+let eti_coords = { "lon": 18.612704654420106, "lat": 54.37165722576064 }
 let stations = {}
-let session_token = "ba4efeda797348a3af96705bac963c7e"
+let session_token = "54db820273fa4c439d4cc78b8e4c18b4"
 let map = 0
 
 async function init() {
-    map = L.map('map').setView([eti_coords.lon, eti_coords.lat], 13);
+    map = L.map('map').setView([eti_coords.lat, eti_coords.lon], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -38,7 +38,10 @@ async function loadStations() {
             record.latitude,
             record.name,
             record.operator,
-            0,
+            record.city,
+            record.street,
+            record.housenumber,
+            record.capacity,
             record.fee)
     }
 }
@@ -55,26 +58,94 @@ function addStationToMap(station) {
         .addTo(map)
 }
 
-function openStationInfo(stationId) {
+async function getRatings(stationId) {
+    let response = await fetch("http://localhost:3011/ratings?token=" + session_token + "&station_id=" + stationId).then(data => data.json())
+
+    if (!response.valid) {
+        alert("Internal application error")
+        return
+    }
+    if (response.length == 0)
+        return new Rates([], response.length)
+    else
+        return new Rates(response.results, response.length)
+}
+
+async function getComments(stationId) {
+    let response = await fetch("http://localhost:3011/comments?token=" + session_token + "&station_id=" + stationId).then(data => data.json())
+
+    if (!response.valid) {
+        alert("Internal application error")
+        return
+    }
+    if (response.length == 0)
+        return new Comments([], response.length)
+    else
+        return new Comments(response.results, response.length)
+}
+
+async function getChargers(stationId) {
+    let response = await fetch("http://localhost:3011/chargers?token=" + session_token + "&station_id=" + stationId).then(data => data.json())
+
+    if (!response.valid) {
+        alert("Internal application error")
+        return
+    }
+    if (response.length == 0)
+        return new Chargers([], response.length)
+    else
+        return new Chargers(response.results, response.length)
+}
+
+function createAcordeonSection(chargerIndex, chargerName) {
+    let acordeonItemName = "ai" + chargerIndex
+    let acordeonHeaderName = 'ah' + chargerIndex
+    let acordeonBoadyId = 'abi' + chargerIndex
+    let acordeonBoadyLabel = 'abl' + chargerIndex
+
+    $("#accordionPanelsChargers").append('<div class="accordion-item" id="' + acordeonItemName + '"></div>')
+    $("#" + acordeonItemName).append('<h2 class="accordion-header" id="' + acordeonHeaderName + '"></h2>')
+    $("#" + acordeonHeaderName).append('<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#' + acordeonBoadyLabel +
+        '" aria-expanded="true" aria-controls="' + acordeonBoadyLabel + '">' + chargerName + '</button>')
+    $("#" + acordeonItemName).append('<div id="' + acordeonBoadyLabel + '" class="accordion-collapse collapse hide" aria-labelledby="' + acordeonHeaderName + '"></div>')
+    $("#" + acordeonBoadyLabel).append('<div class="accordion-body" id="' + acordeonBoadyId + '"></div>')
+
+    return acordeonBoadyId
+}
+
+async function openStationInfo(stationId) {
     let s = stations[stationId]
+    let rates = await getRatings(stationId)
+    let comments = await getComments(stationId)
+    let chargers = await getChargers(stationId)
+
+    $("#accordionPanelsChargers").empty()
 
     $("#stationName").first().text(s.getName())
-    $("#stationRatingsView").first().text("Rating " + s.getRatings() + "/5")
-    $("#stationCommentsLink").first().text("Comments (" + s.getNumberOfComments() + ")")
+    $("#stationRatingsView").first().text("Rating " + rates.getMean().toFixed(1) + "/5")
+    $("#stationCommentsLink").first().text("Comments (" + comments.getNumberOfComments() + ")")
     $("#stationOperatorName").first().text("Operator : " + s.getOperatorName())
-    $("#stationCity").first().text("City : " + s.getOperatorName())
-    $("#stationStreet").first().text("Street : " + s.getOperatorName())
-    $("#stationHouseNumber").first().text("House number : " + s.getOperatorName())
-    $("#stationFee").first().text("Free : " + s.isFree())
+    $("#stationCity").first().text("City : " + s.getCity())
+    $("#stationStreet").first().text("Street : " + s.getStreet())
+    $("#stationHouseNumber").first().text("House number : " + s.getHousenumber())
+    $("#stationFree").first().text("Free : " + s.isFree())
     $("#stationCapacity").first().text("Number of chargers : " + s.getNumberOfChargers())
+
+    // process chargers
+    for (let i in chargers.getChargers()) {
+        let chargerIndex = (parseInt(i) + 1).toString()
+        let chargerName = "Charger " + chargerIndex
+        let bodyItemTag = createAcordeonSection(chargerIndex, chargerName)
+
+        const chargerData = chargers.getChargerData(i)
+        $("#" + bodyItemTag).append('<h6 class="chargerInfo">Voltage: ' + chargerData.voltage + '</h6>')
+        $("#" + bodyItemTag).append('<h6 class="chargerInfo">Amperage: ' + chargerData.amperage + '</h6>')
+        $("#" + bodyItemTag).append('<h6 class="chargerInfo">Plug type: ' + chargerData.plug_type + '</h6>')
+    }
 
     $("#stationInfoModal").modal("show")
 }
 
 function closeStationInfo() {
     $("#stationInfoModal").modal("hide")
-}
-
-function testFunc() {
-    console.log("test")
 }
